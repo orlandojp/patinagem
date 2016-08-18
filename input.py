c69c68b848d8431830c669b2db4210a22620af86
orlandojp <ojpereira@alumni.uminho.pt>
2016-08-18 23:04:02 +0100

    Add files via upload

--- input.py
+++ input.py
@@ -3,8 +3,8 @@ from operator import itemgetter
 from decimal import *
 from collections import defaultdict
 
+listaSaida = []
 listaInicial = []
-listaAtletas = []
 lista = []
 juizes = 3
 
@@ -23,16 +23,18 @@ def carregaAtletas():
     file = open( "atletas.txt", "r" )
     for line in file:
         atleta = line.rstrip().split("\t")
-        listaAtletas.append((int(atleta[0]), atleta[1], Decimal(atleta[2]), Decimal(atleta[3]), Decimal(atleta[4]), Decimal(atleta[5]), Decimal(atleta[6]), Decimal(atleta[7])))
+        listaInicial.append((atleta[0], atleta[1], atleta[2], atleta[3], Decimal(atleta[4]) + Decimal(atleta[5]) + Decimal(atleta[6]), Decimal(atleta[7]) + Decimal(atleta[8]) + Decimal(atleta[9]), Decimal(atleta[7]), Decimal(atleta[8]), Decimal(atleta[9])))
     file.close()
 
 # Le o ficheiro com a ordem de saida dos atletas para receber as notas 
 def leOrdemSaida():
-    file = open( "TorneioVerao.txt", "r" )
+    file = open( "ordem.txt", "r" )
     for line in file:
         no = line.rstrip().split("\t")
-        listaInicial.append((int(no[0]),no[2], Decimal(no[4]) + Decimal(no[7]) + Decimal(no[5]) + Decimal(no[8]) + Decimal(no[6]) + Decimal(no[9]),
-            Decimal(no[4]) + Decimal(no[7]),Decimal(no[5]) + Decimal(no[8]), Decimal(no[6]) + Decimal(no[9]), Decimal(no[7]), Decimal(no[8]), Decimal(no[9])))
+        # listaSaida contem a ordem de saida dos atletas
+        listaSaida.append((no[0],no[1], no[2], no[3]))
+       # listaSaida.append((int(no[0]),no[2], Decimal(no[4]) + Decimal(no[7]) + Decimal(no[5]) + Decimal(no[8]) + Decimal(no[6]) + Decimal(no[9]),
+       #     Decimal(no[4]) + Decimal(no[7]),Decimal(no[5]) + Decimal(no[8]), Decimal(no[6]) + Decimal(no[9]), Decimal(no[7]), Decimal(no[8]), Decimal(no[9])))
     file.close()
 
 # Converte as notas lidas que nao esteja no sistema com decimas para a nota correspondente
@@ -46,7 +48,7 @@ def leNota( ultimo ):
     i = int(ultimo)
     while novoAtleta:
         file = open ("atletas.txt", "a")
-        print 'Atleta: ', listaInicial[i][1]
+        print 'Atleta: ', listaSaida[i][2], listaSaida[i][3]
         print 'Nota A'
         nota1 = Decimal(input("Juiz 1: "))
         nota1 = converteNota (nota1)
@@ -61,16 +63,18 @@ def leNota( ultimo ):
         nota5 = converteNota (nota5)
         nota6 = Decimal(raw_input("Juiz 3: "))
         nota6 = converteNota (nota6)
-        continuar = raw_input("Novo atleta? ")
-        if continuar != 's':
-            novoAtleta = False
-        listaAtletas.append((listaInicial[i][1],nota1,nota2,nota3,nota4,nota5,nota6))
-        texto = str(listaInicial[i][0]) + '\t' + listaInicial[i][1] + '\t' + str(listaAtletas[i][1]) + '\t' + str(listaAtletas[i][2]) + '\t' + str(listaAtletas[i][3]) + '\t' + str(listaAtletas[i][4]) + '\t' + str(listaAtletas[i][5]) + '\t' + str(listaAtletas[i][6]) + '\n'
+        # listaInicial composta por num, num da fpp, nome, clube, soma notas A, somas notas B, notas B discriminadas
+        listaInicial.append(( listaSaida[i][0], listaSaida[i][1], listaSaida[i][2], listaSaida[i][3], nota1 + nota2 + nota3, nota4 + nota5 + nota6, nota4, nota5, nota6 ))
+        texto = listaSaida[i][0] + '\t' + listaSaida[i][1] + '\t' + listaSaida[i][2] + '\t' + listaSaida[i][3] + '\t' + str(nota1) + '\t' + str(nota2) + '\t' + str(nota3) + '\t' + str(nota4) + '\t' + str(nota5) + '\t' + str(nota6) + '\n'
+        print texto
         file.write(texto)
         i += 1
         file.close()
+        continuar = raw_input("Novo atleta? ")
+        if continuar != 's':
+            novoAtleta = False
 
-    print listaAtletas
+    print listaInicial
 
 ultimo = atletasCompetiram("atletas.txt")
 if ultimo != 0:
